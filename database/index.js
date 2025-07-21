abfa7da378eb1e57c5a413ffe45e7aefa9a0011e
AdongoNat <danieladongonat@gmail.com>
2025-07-21 10:20:27 +0000

    test 2

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ require("dotenv").config()
  * Connection Pool
  * SSL Object needed for local testing of app
  * But will cause problems in production environment
- * If - else    will make determination which to use
+ * If - else    will   make determination which to use
  * *************** */
 let pool
 if (process.env.NODE_ENV == "development") {
